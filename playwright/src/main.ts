07a22492e19937a89fbf621e8389a83fd6740ce4
Tim Denning <36028402+denningta@users.noreply.github.com>
2024-10-17 05:10:19 -0600

    simple commit

--- playwright/src/main.ts
+++ playwright/src/main.ts
@@ -14,12 +14,26 @@ async function main() {
   await browser.close();
 }
 
+async function addOperations(url: string, operations: string[]) {
+  console.log(url)
+  console.log(operations)
+
+}
+
+
 const command = process.argv[2]
 
 switch (command) {
   case 'run':
     main().catch(e => console.error(e))
     break;
+
+  case 'add-ops':
+    const url = process.argv[3]
+    const operations = process.argv[4].split(',')
+    addOperations(url, operations).catch(e => console.error(e))
+    break;
+
   default:
     console.error(`unknown command ${command}`)
     process.exit(1)
